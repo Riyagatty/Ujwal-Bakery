0cadd593bd3127f5133103a5638e5c7710256c62
Riyagatty <riyagatty26@gmail.com>
2024-10-07 00:20:14 +0530

    navbar completed

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,11 @@
+import bakery from './bakery.png'
+import logo from './logo.png'
+import looking from './looking.png'
+import userlogin from './userlogin.png'
+
+export const assets={
+    bakery,
+    logo,
+    looking,
+    userlogin,
+}
---
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+const Navbar = () => {
+    const[menu,setMenu]=useState("Home");
+    
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className="logo"/> 
+      <ul className="navbar-menu">
+        <li onClick={()=>setMenu("Home")}className={menu==="Home"?"active":""}>Home</li>
+        <li onClick={()=>setMenu("Menu")}className={menu==="Menu"?"active":""}>Menu</li>
+        <li onClick={()=>setMenu("About us")}className={menu==="About us"?"active":""}>About us</li>
+        <li onClick={()=>setMenu("Contact us")}className={menu==="Contact us"?"active":""}>Contact us</li>
+      </ul>
+      <div className="navbar-right">
+      <img src={assets.looking} alt="" /> 
+      <div className='navbar-search-icon'>
+      <img src={assets.bakery} alt=""/> 
+      <div className="dot"></div>
+      </div>
+      <button>Sign in</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar

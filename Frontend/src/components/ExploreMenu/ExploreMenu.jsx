c5e52e86f7b1f4fd2e2477aa9ff7aba8b89d9742
Riyagatty <riyagatty26@gmail.com>
2024-11-10 23:27:22 +0530

    Co-authored-by: Wiola Lobo <wiolaaj@users.noreply.github.com>

--- Frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ Frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import "./ExploreMenu.css"
-const ExploreMenu = () => {
+import { menu_list } from '../../assets/assets'
+const ExploreMenu = ({category,setCategory}) => {
   return (
-    <div>
-      
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu </h1>
+      <p className='explore-menu-text'>Explore our delicious assortment of freshly baked goods, crafted with the finest ingredients to bring warmth and flavor to every bite. From classic cakes to savory treats, there’s something for everyone to enjoy!</p>
+      <div className='explore-menu-list'>
+        {menu_list.map((item,index)=>{
+          return(
+            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                <img  className={category===item.menu_name?"active":""} src={item.menu_image}alt=''/>
+                <p>{item.menu_name}</p>
+
+            </div>
+        )
+        })}
+      </div>
+      <hr></hr>
     </div>
   )
 }
--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -1,25 +1,30 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-const Navbar = () => {
-    const[menu,setMenu]=useState("Home");
+import {Link} from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
+const Navbar = ({setShowLogin}) => {
+  
+    const[menu,setMenu]=useState("menu");
+
+    const {getTotalCartAmount}=useContext(StoreContext);
     
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="" className="logo"/> 
+      <Link to='/'><img src={assets.logo} alt="" className="logo"/> </Link>
       <ul className="navbar-menu">
-        <li onClick={()=>setMenu("Home")}className={menu==="Home"?"active":""}>Home</li>
-        <li onClick={()=>setMenu("Menu")}className={menu==="Menu"?"active":""}>Menu</li>
-        <li onClick={()=>setMenu("About us")}className={menu==="About us"?"active":""}>About us</li>
-        <li onClick={()=>setMenu("Contact us")}className={menu==="Contact us"?"active":""}>Contact us</li>
+        <Link to='/' onClick={()=>setMenu("Home")}className={menu==="Home"?"active":""}>Home</Link>
+        <a href='#explore-menu' onClick={()=>setMenu("Menu")}className={menu==="Menu"?"active":""}>Menu</a>
+        <a href='#app-download' onClick={()=>setMenu("Mobile App")}className={menu==="Mobile App"?"active":""}>Mobile App</a>
+        <a href='#footer' onClick={()=>setMenu("Contact us")}className={menu==="Contact us"?"active":""}>Contact us</a>
       </ul>
       <div className="navbar-right">
       <img src={assets.looking} alt="" /> 
       <div className='navbar-search-icon'>
-      <img src={assets.bakery} alt=""/> 
-      <div className="dot"></div>
+      <Link to='/cart'><img src={assets.bakery} alt=""/></Link>
+      <div className={getTotalCartAmount()===0?"":"dot"}></div>
       </div>
-      <button>Sign in</button> 
+      <button onClick={()=>setShowLogin(true)}>Sign in</button> 
       </div>
 
     </div>
